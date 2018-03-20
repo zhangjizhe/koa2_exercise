dc3bb2a0e285fee1dc41ac610825903208b814f0
zhangjizhe <zhang@1992>
2018-03-20 16:00:33 +0800

    d
    Signed-off-by: zhangjizhe <zhang@1992>

---
+++ ejs-index.js
@@ -0,0 +1,20 @@
+const Koa = require('koa')
+const views = require('koa-views')
+const path = require('path')
+const app = new Koa()
+
+// 加载模板引擎
+app.use(views(path.join(__dirname, './view'), {
+  extension: 'ejs'
+}))
+
+app.use( async ( ctx ) => {
+  let title = 'hello koa2'
+  let word = '测试第一类'
+  await ctx.render('index', {
+    title,
+    word
+  })
+})
+
+app.listen(3000)
